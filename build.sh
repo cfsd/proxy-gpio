32edb57bd4d97bbf1e11d6433fac7a14b809d47f
Dan Andersson <andedan@student.chalmers.se>
2018-05-09 16:55:46 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+name="proxy-lynx-gpio-v0.0.1"
+tag=$(git log -1 --pretty=%h)
+img="$name:$tag"
+echo $img
+docker build -t $img -f Dockerfile.armhf .
 
-docker build -t proxy-lynx-gpio-v0.0.1 -f Dockerfile.armhf .
-docker save  proxy-lynx-gpio-v0.0.1 > proxy-lynx-gpio-v0.0.1.tar
+docker save  $img > $img.tar
